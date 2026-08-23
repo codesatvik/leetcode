0c231d401d80af1a0c7a552576e23d96ba56c4be
Satvik <codesatvik@gmail.com>
2026-08-23 16:25:22 +0530

    spawn to compile and run code of py cpp and js

---
+++ worker/code/a.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+int main(){std::cout<<"Hello Worl22d";return 0;}
--- worker/index.ts
+++ worker/index.ts
@@ -1,4 +1,6 @@
 import { createClient } from "redis";
+import fs from "fs"
+import { spawn } from "child_process";
 
 const client = createClient()
 client.connect()
@@ -13,14 +15,39 @@ client.connect()
             const code = parsedResponse.code;
             const language = parsedResponse.language;
 
-            if (language === "c++") {
+            if (language === "cpp") {
                 console.log("running users c++ code")
-                await new Promise((r) => setTimeout(r, 10000))
+                const filePath = __dirname + "/code/a.cpp";
+                const outPath = __dirname + "/code/out";
+                fs.writeFileSync(filePath, code);
+                 await new Promise<void>((resolve, reject) => {
+                  const compile = spawn("g++", [filePath, "-o", outPath]);
+                  compile.on("close", (code) => code === 0 ? resolve() : reject(new Error("Compilation failed")));
+                 });
+
+                const response = spawn("./code/out");
+                response.stdout.on("data", (chunk) => { 
+                    console.log(chunk.toString())
+                })
             }
             
             if (language === "js") {
                 console.log("running users js code")
-                await new Promise((r) => setTimeout(r, 10000))
+                const filePath = __dirname + "/code/a.js";
+                fs.writeFileSync(filePath, code);
+                const response = spawn("node", [filePath]);
+                response.stdout.on("data", (chunk) => { 
+                    console.log(chunk.toString());
+                })
+            }
+             if (language === "py") {
+                console.log("running users py code")
+                const filePath = __dirname + "/code/a.py";
+                fs.writeFileSync(filePath, code);
+                const response = spawn("python", [filePath]);
+                response.stdout.on("data", (chunk) => { 
+                    console.log(chunk.toString());
+                })
             }
         }
     })
