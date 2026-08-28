ff1e581c7b2743354ad36b3399f7bdf36b7d96d8
Satvik <codesatvik@gmail.com>
2026-08-28 16:19:23 +0530

    initialize frontend with Bun , React , shadcn and tailwindcss setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+
+import "./index.css";
+
+
+export function App() {
+  return <div className="h-screen w-screen flex">
+    <div className="flex-1 h-screen">
+      <textarea className="h-screen w-full" rows={500}>
+
+      </textarea>
+    </div>
+    <div className="flex-1 h-screen bg-green-300">
+
+    </div>
+  </div>
+}
+
+export default App;
